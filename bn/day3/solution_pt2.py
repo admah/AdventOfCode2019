30ae8c4eba03072bc5875e6caa5ea5422996a4ff
Benjamin Ness <benjamin.ness@gdit.com>
2019-12-03 16:54:47 -0600

    works, but allocating the huge grid is SLOOOOOW

---
+++ bn/day3/solution_pt2.py
@@ -0,0 +1,54 @@
+filepath = 'input'
+with open(filepath) as fp:
+   line = fp.readline().strip()
+   wire_1 = line.split(',')
+   line = fp.readline().strip()
+   wire_2 = line.split(',')
+
+   grid_dim = 50001
+   grid = [[0] * grid_dim for i in range(grid_dim)]
+   current_x = grid_dim/2
+   current_y = grid_dim/2
+   steps = 0
+   while len(wire_1) > 0:
+     instruction = wire_1.pop(0)
+     direction = instruction[0:1]
+     distance = int(instruction[1:])
+     while distance > 0:
+       steps += 1
+       if direction == 'U':
+         current_y -= 1
+       if direction == 'D':
+         current_y += 1
+       if direction == 'L':
+         current_x -= 1
+       if direction == 'R':
+         current_x += 1
+       if grid[current_x][current_y] == 0:
+         grid[current_x][current_y] = steps
+       distance -= 1
+
+   min_dist = 100000
+   current_x = grid_dim/2
+   current_y = grid_dim/2
+   steps = 0
+   while len(wire_2) > 0:
+     instruction = wire_2.pop(0)
+     direction = instruction[0:1]
+     distance = int(instruction[1:])
+     while distance > 0:
+       steps += 1
+       if direction == 'U':
+         current_y -= 1
+       if direction == 'D':
+         current_y += 1
+       if direction == 'L':
+         current_x -= 1
+       if direction == 'R':
+         current_x += 1
+       if grid[current_x][current_y] > 0:
+         cross_distance = grid[current_x][current_y] + steps
+         min_dist = min(cross_distance, min_dist)
+       distance -= 1
+
+   print(min_dist)
